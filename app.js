a370b715d66113aa1a2a0156bc5ce90f90ee5b72
omiras <oscar.miras@gmail.com>
2022-03-28 09:43:22 +0200

    updated

--- app.js
+++ app.js
@@ -6,21 +6,3 @@ let secondNumber = chance.integer({ min: 0, max: 10 })
 
 //get the total
 let total = firstNumber + secondNumber;
-
-//display numbers on the canvas
-$('#primary-number').text(firstNumber);
-$('#secondary-number').text(secondNumber);
-
-$('#btn').click(checkSum);
-
-function checkSum() {
-    let value = $('#guess').val();
-
-    if (value == total) {
-        alert("Correct!");
-    }
-
-    else {
-        alert("Wrong!")
-    }
-}
