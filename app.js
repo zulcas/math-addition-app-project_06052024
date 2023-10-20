f83ab6d7e4c81fdee9e360e1ec215b64054cdfa6
Oscar Miras <oscar.miras@gmail.com>
2023-10-20 13:05:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-//generate random numbers. Utilizo la biblioteca Chance !
-// https://chancejs.com/basics/integer.html
-
-let firstNumber = chance.integer({ min: 0, max: 10 })
-
-let secondNumber = chance.integer({ min: 0, max: 10 })
-
-//get the total
-let total = firstNumber + secondNumber;
+// Buena suerte!
